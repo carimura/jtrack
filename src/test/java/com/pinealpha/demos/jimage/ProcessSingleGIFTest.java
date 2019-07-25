399d86dd326b6e08cf1f1d0fb146c098ea94674e
Denis Makogon <denys.makogon@oracle.com>
2019-07-25 19:47:18 +0300

    Better GIF processing
     - first of all - tests!!
     - wrap FaceDetect constructor into fiber and instantiate it once at the very beginning

---
+++ src/test/java/com/pinealpha/demos/jimage/ProcessSingleGIFTest.java
@@ -0,0 +1,33 @@
+package com.pinealpha.demos.jimage;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static junit.framework.TestCase.*;
+
+public class ProcessSingleGIFTest {
+
+    @Test
+    public void testSingleGIF() {
+        try {
+            var faceDetect = new FaceDetect();
+            var usableURL = "http://www.dailyhaha.com/_gifs/making-some-fun-faces.gif";
+            var storeFolder = "/tmp/";
+            App.processSingleGIF(faceDetect, usableURL, storeFolder);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } finally {
+            try {
+                Files.deleteIfExists(Paths.get("/tmp/original.gif"));
+                Files.deleteIfExists(Paths.get("/tmp/final.gif"));
+            } catch (IOException e) {
+                // do nothing
+            }
+        }
+    }
+}
