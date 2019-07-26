527039d1f5214bd7fd9a526b6265573878474a42
Chad Arimura <carimura@gmail.com>
2019-07-26 06:35:35 -0700

    revert tests back

--- src/test/java/com/pinealpha/demos/jimage/FaceDetectInitTest.java
+++ src/test/java/com/pinealpha/demos/jimage/FaceDetectInitTest.java
@@ -8,17 +8,17 @@ public class FaceDetectInitTest {
 
     @Test
     public void testInit() {
-//        var faceDetect = new FaceDetect();
-//        assertFalse(faceDetect.isInitialized());
-//
-//        try {
-//            // classifier obtaining happens in a background
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//            fail();
-//        }
-//        assertTrue(faceDetect.isInitialized());
+        var faceDetect = new FaceDetect();
+        assertFalse(faceDetect.isInitialized());
+
+        try {
+            // classifier obtaining happens in a background
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail();
+        }
+        assertTrue(faceDetect.isInitialized());
     }
 
 }
--- src/test/java/com/pinealpha/demos/jimage/ProcessSingleGIFTest.java
+++ src/test/java/com/pinealpha/demos/jimage/ProcessSingleGIFTest.java
@@ -12,22 +12,22 @@ public class ProcessSingleGIFTest {
 
     @Test
     public void testSingleGIF() {
-//        try {
-//            var faceDetect = new FaceDetect();
-//            var usableURL = "http://www.dailyhaha.com/_gifs/making-some-fun-faces.gif";
-//            var storeFolder = "/tmp/";
-//            App.processSingleGIF(faceDetect, usableURL, storeFolder);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            fail();
-//        } finally {
-//            try {
-//                Files.deleteIfExists(Paths.get("/tmp/original.gif"));
-//                Files.deleteIfExists(Paths.get("/tmp/final.gif"));
-//            } catch (IOException e) {
-//                // do nothing
-//            }
-//        }
+        try {
+            var faceDetect = new FaceDetect();
+            var usableURL = "http://www.dailyhaha.com/_gifs/making-some-fun-faces.gif";
+            var storeFolder = "/tmp/";
+            App.processSingleGIF(faceDetect, usableURL, storeFolder);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } finally {
+            try {
+                Files.deleteIfExists(Paths.get("/tmp/original.gif"));
+                Files.deleteIfExists(Paths.get("/tmp/final.gif"));
+            } catch (IOException e) {
+                // do nothing
+            }
+        }
     }
 }
